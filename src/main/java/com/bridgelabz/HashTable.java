e02ebd6d737fd4e94ba7d5cbb4c6c1ad9f127a5a
lokeshbs1994 <lokeshbs619@gmail.com>
2022-03-20 16:30:35 +0530

    Added: Frequency Of Large Paragraph Phrase Using Hash Table

--- src/main/java/com/bridgelabz/HashTable.java
+++ src/main/java/com/bridgelabz/HashTable.java
@@ -1,8 +1,9 @@
-/*@Purpose : Ability to find frequency of words in a sentence like “To be or not to be”
- *- Use LinkedList to do the Hash Table Operation
- *-To do this we create MyMapNode
- * with Key Value Pair and create
- * LinkedList of MyMapNode
+/*@Purpose : Ability to find frequency of words in a large paragraph phrase “Paranoids are not
+ * paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations”
+ * - Use hashcode to find index of the words in the para
+ * - Create LinkedList for each index and store the words and its frequency
+ * - Use LinkedList to do the Hash Table Operation
+ * - To do this create MyMapNode with Key Value Pair and create LinkedList of MyMapNode
  * @File : Data Structure Using Hash Table
  * @Author : Lokesh B S
  */
@@ -12,7 +13,7 @@ public class HashTable {
     public static void main(String[] args) {
         System.out.println("Welcome To Hash Table Program");
         HashTableImplementation<String, Integer> hashImpl = new HashTableImplementation<String, Integer>();
-        String message = "To be or not to be";
+        String message = "Paranoids are not	paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
         // Split the words when a white space comes up and Convert all the words in
         // lowercase
--- src/main/java/com/bridgelabz/HashTableImplementation.java
+++ src/main/java/com/bridgelabz/HashTableImplementation.java
@@ -1,8 +1,9 @@
-/*@Purpose : Ability to find frequency of words in a sentence like “To be or not to be”
- *- Use LinkedList to do the Hash Table Operation
- *-To do this we create MyMapNode
- * with Key Value Pair and create
- * LinkedList of MyMapNode
+/*@Purpose : Ability to find frequency of words in a large paragraph phrase “Paranoids are not
+ * paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations”
+ * - Use hashcode to find index of the words in the para
+ * - Create LinkedList for each index and store the words and its frequency
+ * - Use LinkedList to do the Hash Table Operation
+ * - To do this create MyMapNode with Key Value Pair and create LinkedList of MyMapNode
  * @File : Data Structure Using Hash Table
  * @Author : Lokesh BS
  */
